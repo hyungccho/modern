62e5e8b815ba645450bf7f98b5f1f97007da5cc2
hyungccho <hyungchulcho93@gmail.com>
2016-09-14 23:04:34 -0700

    Add webpack prod env to config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
@@ -19,6 +20,13 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+  ],
   devtool: 'source-maps',
   resolve: {
     extensions: ["", ".js", ".jsx" ],
